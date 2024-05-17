e5feaad85dc0fb6fce5bdaeea0459168f2de97e5
hijab muallif <newhijabmuallif@gmail.com>
2024-05-17 17:33:07 +0800

    feat: add unit test

--- src/modules/book/book.service.spec.ts
+++ src/modules/book/book.service.spec.ts
@@ -1,12 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookService } from './book.service';
+import { Book } from './entities/book.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+
+const mockBook: Book = {
+  id: 1,
+  author: 'author',
+  title: 'title',
+};
+
+const mockBookRepository = {
+  find: jest.fn().mockResolvedValue([mockBook]),
+  findOneBy: jest.fn().mockResolvedValue(mockBook),
+  save: jest.fn().mockResolvedValue(mockBook),
+  create: jest.fn(),
+  update: jest.fn(),
+  findOneByOrFail: jest
+    .fn()
+    .mockResolvedValue({
+      id: '',
+    })
+    .mockResolvedValue(mockBook),
+  findOneOrFail: jest
+    .fn()
+    .mockResolvedValue({
+      id: '',
+    })
+    .mockResolvedValue(mockBook),
+  delete: jest.fn(),
+};
 
 describe('BookService', () => {
   let service: BookService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BookService],
+      providers: [
+        BookService,
+        {
+          provide: getRepositoryToken(Book),
+          useValue: mockBookRepository,
+        },
+        {
+          provide: WINSTON_MODULE_PROVIDER,
+          useValue: {
+            debug: jest.fn().mockResolvedValue(''),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<BookService>(BookService);
@@ -14,5 +56,39 @@ describe('BookService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be able to findAll', async () => {
+    await expect(service.findAll()).resolves.toEqual([mockBook]);
+
+    expect(mockBookRepository.find).toBeCalled();
+  });
+
+  it('should be able to findOne', async () => {
+    await expect(service.findOne(mockBook.id)).resolves.toEqual(mockBook);
+
+    expect(mockBookRepository.findOneByOrFail).toBeCalled();
+  });
+
+  it('should be able to create', async () => {
+    await expect(service.create(mockBook)).resolves.toEqual(mockBook);
+
+    expect(mockBookRepository.save).toBeCalled();
+    expect(mockBookRepository.create).toBeCalled();
+  });
+
+  it('should be able to update', async () => {
+    await expect(service.update(mockBook.id, mockBook)).resolves.toEqual(
+      mockBook,
+    );
+
+    expect(mockBookRepository.save).toBeCalled();
+    expect(mockBookRepository.create).toBeCalled();
+  });
+
+  it('should be able to delete', async () => {
+    await expect(service.remove(mockBook.id)).resolves.toEqual(mockBook);
+
+    expect(mockBookRepository.delete).toBeCalled();
   });
 });
--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -1,12 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from './entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Role } from 'src/types';
 import { UserService } from './user.service';
 
-describe('UserService', () => {
+const mockUser: User = {
+  email: 'email@email.com',
+  id: 1,
+  name: 'name',
+  password: '123456',
+  role: Role.USER,
+  setPassword: async () => {},
+};
+
+const mockUserRepository = {
+  find: jest.fn().mockResolvedValue([mockUser]),
+  findOneBy: jest.fn().mockResolvedValue(mockUser),
+  save: jest.fn().mockResolvedValue(mockUser),
+  create: jest.fn(),
+  update: jest.fn(),
+  findOneByOrFail: jest
+    .fn()
+    .mockResolvedValue({
+      id: '',
+    })
+    .mockResolvedValue(mockUser),
+  findOneOrFail: jest
+    .fn()
+    .mockResolvedValue({
+      id: '',
+    })
+    .mockResolvedValue(mockUser),
+  delete: jest.fn(),
+};
+
+describe('service', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
@@ -14,5 +53,17 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be able to findAll', async () => {
+    await expect(service.findAll()).resolves.toEqual([mockUser]);
+
+    expect(mockUserRepository.find).toBeCalled();
+  });
+
+  it('should be able to findOne', async () => {
+    await expect(service.findOne(mockUser.id)).resolves.toEqual(mockUser);
+
+    expect(mockUserRepository.findOneByOrFail).toBeCalled();
   });
 });
