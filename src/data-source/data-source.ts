64aa594fe61c51c564e52b4f8c0eb391e5a2dd16
hijab muallif <newhijabmuallif@gmail.com>
2024-05-17 14:00:00 +0800

    feat: initialize project with modules user and auth

---
+++ src/data-source/data-source.ts
@@ -0,0 +1,22 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+export const safeInit = (dataSource: DataSource) =>
+  !dataSource.isInitialized ? dataSource.initialize() : dataSource;
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'sqlite',
+  database: 'db/sql.db',
+  // host: 'localhost',
+  // port: 3306,
+  // username: 'root',
+  //   password: '',
+  //   autoLoadEntities: true,
+  // entities: [Book, Member, Borrowing],
+  entities: [],
+  migrations: ['dist/migrations/*{.ts,.js}'],
+  synchronize: false,
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+
+export default dataSource;
